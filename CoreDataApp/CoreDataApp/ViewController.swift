0f83f089ff07db4190848f3561d19cb6dd794ba7
Dmitry Satskevich <Satsckevich1993@yandex.ru>
2023-01-06 17:52:37 +0300

    Project created

---
+++ CoreDataApp/CoreDataApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataApp
+//
+//  Created by dzmitry on 6.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
